f9e3d08696f11eac8061255dedeb2222bb8bd892
ARJUN <pachangedinanath@gmail.com>
2025-07-24 19:38:59 +0530

    Update play.py

--- BABYMUSIC/cplugin/play.py
+++ BABYMUSIC/cplugin/play.py
@@ -12,11 +12,12 @@ from BABYMUSIC.utils.decorators.play import CPlayWrapper
 from BABYMUSIC.utils.database import add_served_chat_clone
 from BABYMUSIC.utils.logger import play_logs
 
-# Anti-spam tracker
+# Anti-spam trackers
 user_last_message_time = {}
 user_command_count = {}
-SPAM_THRESHOLD = 2
-SPAM_WINDOW_SECONDS = 5
+
+SPAM_THRESHOLD = 2  # max commands
+SPAM_WINDOW_SECONDS = 5  # time window
 
 @app.on_message(
     filters.command(
@@ -30,12 +31,15 @@ async def play_command(client, message: Message, _, chat_id, video, channel, pla
     user_id = message.from_user.id
     current_time = time()
 
-    # Anti-spam logic
-    if current_time - user_last_message_time.get(user_id, 0) < SPAM_WINDOW_SECONDS:
-        user_command_count[user_id] = user_command_count.get(user_id, 0) + 1
+    # Spam protection
+    last_time = user_last_message_time.get(user_id, 0)
+    command_count = user_command_count.get(user_id, 0)
+
+    if current_time - last_time < SPAM_WINDOW_SECONDS:
+        user_command_count[user_id] = command_count + 1
         if user_command_count[user_id] > SPAM_THRESHOLD:
             warn = await message.reply_text(
-                f"**{message.from_user.mention} ᴘʟᴇᴀsᴇ ᴅᴏɴ'ᴛ sᴘᴀᴍ, ᴡᴀɪᴛ 5 sᴇᴄᴏɴᴅs**"
+                f"**{message.from_user.mention}**, ᴘʟᴇᴀsᴇ ᴅᴏɴ'ᴛ sᴘᴀᴍ. ᴡᴀɪᴛ 5 sᴇᴄᴏɴᴅs."
             )
             await asyncio.sleep(3)
             return await warn.delete()
@@ -44,17 +48,16 @@ async def play_command(client, message: Message, _, chat_id, video, channel, pla
 
     user_last_message_time[user_id] = current_time
 
-    # Mark this chat as served
+    # Log and update database
     await add_served_chat_clone(message.chat.id)
 
-    # Send loading message
-    mystic = await message.reply_text(
-        _["play_2"].format(channel) if channel else _["play_1"]
-    )
+    # Reply loading message
+    loading_msg = _["play_2"].format(channel) if channel else _["play_1"]
+    mystic = await message.reply_text(loading_msg)
 
-    from BABYMUSIC.utils.stream.stream import stream  # import inside
-
-    # You can continue with search, streaming, or Telegram file playback logic here...
-    # Like: await stream(...)
+    # Proceed to stream logic
+    from BABYMUSIC.utils.stream.stream import stream
+    # Add your actual stream logic here
+    # Example: await stream(...)
 
     return
